4601e1f3fb5e68cbeb2f4f0093f9440be7db13b5
Csanád Katona <kcsanad314@gmail.com>
2022-07-20 18:25:48 +0200

    creation of base models

---
+++ InspectionAPI/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI.Models
+{
+    public class Food
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int PreparationTime { get; set; }
+        public string Allergenes { get; set; }
+        public double DiscountMultiplier { get; set; } = 1;
+        public int FoodCategoryId { get; set; } 
+    }
+}
